b77eab1d0341a33ab23688fa875d458e1d83e7bb
Giacomo Ciccone <giacomo98ciccone@gmail.com>
2023-05-05 11:05:22 +0200

    Project setup

---
+++ src/main/java/administrator/server/infrastructure/JerseyServer.java
@@ -0,0 +1,7 @@
+package administrator.server.infrastructure;
+
+public class JerseyServer {
+    private static final String BASE_URI = "http://localhost:8080/";
+
+
+}
---
+++ src/main/java/robot/Robot.java
@@ -0,0 +1,9 @@
+package robot;
+
+import utils.Logger;
+
+public class Robot {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
